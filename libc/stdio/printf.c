b68172714331d44a1dacc3a4c34e3953c48727c6
philipandag <philipgoclever@gmail.com>
2023-10-29 14:58:26 +0100

    added support for %ld, %lld, %lx and %llx formats

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -54,6 +54,7 @@ int printf(const char* restrict format, ...){
             if(!print(&c, sizeof(c)))
                 return -1;
             written++;
+
         } else if(*format == 's'){
             format++;
             const char* str = va_arg(parameters, const char*);
@@ -65,10 +66,11 @@ int printf(const char* restrict format, ...){
             if(!print(str, len))
                 return -1;
             written += len;
+
         } else if (*format == 'd') { // TODO: make it signed
             format++;
-            const unsigned int num = va_arg(parameters, unsigned int);
-            char buf[11];
+            const int num = va_arg(parameters, const int);
+            char buf[11]; // INT_MAX is 10 digits in base 10 4294967296
             buf[10] = '\0';
             itoa(num, buf, 10);
             size_t len = strlen(buf);
@@ -79,10 +81,41 @@ int printf(const char* restrict format, ...){
             if(!print(buf, len))
                 return -1;
             written += len;
+
+        } else if(*format == 'l' && *(format+1) == 'd') {
+            format += 2;
+            const long long num = va_arg(parameters, const long);
+            char buf[20]; // 2^64 is 19 digits in base 10 9223372036854775807
+            buf[19] = '\0';
+            ltoa(num, buf, 10);
+            size_t len = strlen(buf);
+            if(maxrem < len){
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if(!print(buf, len))
+                return -1;
+            written += len;
+
+        } else if(*format == 'l' && *(format+1) == 'l' && *(format+2) == 'd') {
+            format += 3;
+            const long long num = va_arg(parameters, const long long);
+            char buf[20]; // 2^64 is 19 digits in base 10 9223372036854775807
+            buf[19] = '\0';
+            lltoa(num, buf, 10);
+            size_t len = strlen(buf);
+            if(maxrem < len){
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if(!print(buf, len))
+                return -1;
+            written += len;
+
         } else if(*format == 'x') {
             format++;
-            const unsigned int num = va_arg(parameters, unsigned int);
-            char buf[11];
+            const int num = va_arg(parameters, const int);
+            char buf[9]; // INT_MAX is 8 digits in base 16 FFFFFFFF
             buf[10] = '\0';
             itoa(num, buf, 16);
             size_t len = strlen(buf);
@@ -93,11 +126,12 @@ int printf(const char* restrict format, ...){
             if(!print(buf, len))
                 return -1;
             written += len;
-        } else if(*format == 'l' && *(format+1) == 'd') {
+
+        } else if(*format == 'l' && *(format+1) == 'x') {
             format += 2;
-            const unsigned int num = va_arg(parameters, unsigned int);
-            char buf[11];
-            buf[10] = '\0';
+            const long num = va_arg(parameters, const long);
+            char buf[17]; // 2^64 is 16 digits in base 16 FFFFFFFFFFFFFFFF
+            buf[16] = '\0';
             ltoa(num, buf, 16);
             size_t len = strlen(buf);
             if(maxrem < len){
@@ -107,6 +141,22 @@ int printf(const char* restrict format, ...){
             if(!print(buf, len))
                 return -1;
             written += len;
+
+        } else if(*format == 'l' && *(format+1) == 'l' && *(format+2) == 'x') {
+            format += 3;
+            const long long num = va_arg(parameters, const long long);
+            char buf[17]; // 2^64 is 16 digits in base 16 FFFFFFFFFFFFFFFF
+            buf[16] = '\0';
+            lltoa(num, buf, 16);
+            size_t len = strlen(buf);
+            if(maxrem < len){
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if(!print(buf, len))
+                return -1;
+            written += len;
+
         } else {
             format = format_begun_at;
             size_t len = strlen(format);
